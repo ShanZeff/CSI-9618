8902aaea79c0b0452e774c05c2a0faa32a2d63d4
ShanZeff <18shanzeff4@gmail.com>
2022-02-24 22:03:28 +0800

    first round

--- Task 23.05_nurshanaya.py
+++ Task 23.05_nurshanaya.py
@@ -27,10 +27,78 @@ def insert_node(list, start_pointer, free_list_ptr, new_item):
         while this_node_ptr != null_pointer and list[this_node_ptr].data < new_item:
             previous_node_ptr = this_node_ptr
             this_node_ptr = list[this_node_ptr].pointer
-
         if previous_node_ptr == null_pointer:
             list[new_node_ptr].pointer = start_pointer
             start_pointer = new_node_ptr
         else:
             list[new_node_ptr].pointer = list[previous_node_ptr].pointer
             list[previous_node_ptr].pointer = new_node_ptr
+    else:
+        print("no space for more data")
+    return list, start_pointer, free_list_ptr
+
+
+def find_node(list, start_pointer, data_item):
+    current_node_ptr = start_pointer
+    while current_node_ptr != null_pointer and list[current_node_ptr].data != data_item:
+        current_node_ptr = list[current_node_ptr].pointer
+    return current_node_ptr
+
+
+def delete_node(list, start_pointer, free_list_ptr, data_item):
+    this_node_ptr = start_pointer
+    while this_node_ptr != null_pointer and list[this_node_ptr].data_item:
+        previous_node_ptr = this_node_ptr
+        this_node_ptr = list[this_node_ptr].pointer
+    if this_node_ptr != null_pointer:
+        if this_node_ptr == start_pointer:
+            start_pointer == list[start_pointer].pointer
+        else:
+            list[previous_node_ptr].pointer = list[this_node_ptr].pointer
+        list[this_node_ptr].pointer = free_list_ptr
+        free_list_ptr = this_node_ptr
+    else:
+        print("data does not exist in list")
+    return list, start_pointer, free_list_ptr
+
+
+def output_all_nodes(list, start_pointer):
+    current_node_ptr = start_pointer
+    if start_pointer == null_pointer:
+        print("no data in list")
+    while current_node_ptr != null_pointer:
+        print(current_node_ptr, " ", list[current_node_ptr].data)
+        current_node_ptr = list[current_node_ptr].pointer
+
+
+def get_option():
+    print("1: insert a value")
+    print("2: delete a value")
+    print("3: find a value")
+    print("4: output list")
+    print("5: end program")
+    option = input("Enter your choice: ")
+    return option
+list, start_pointer, free_list_ptr = initialise_list()
+
+option = get_option()
+while option != "5":
+    if option == "1":
+        data = input("Enter the value: ")
+        list, start_pointer, free_list_ptr = insert_node(list, start_pointer, free_list_ptr, data)
+        output_all_nodes(list, start_pointer)
+    elif option == "2":
+        data = input("Enter the value: ")
+        list, start_pointer, free_list_ptr = delete_node(list, start_pointer, free_list_ptr, data)
+        output_all_nodes(list, start_pointer)
+    elif option == "3":
+        data = input("Enter the value: ")
+        current_node_ptr = find_node(list, start_pointer, data)
+        if current_node_ptr == null_pointer:
+            print("data not found")
+        print(start_pointer, free_list_ptr)
+        for i in range(8):
+            print(i, " ", list[i].data, " ", list[i].pointer)
+    elif option == "4":
+        output_all_nodes(list, start_pointer)
+    option = get_option()
