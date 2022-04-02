2bb597c4c596debd467dd1dd1fdafd31990cb67c
ShanZeff <18shanzeff4@gmail.com>
2022-04-03 00:22:43 +0800

    update

--- Task 24.01_nurshanaya.py
+++ Task 24.01_nurshanaya.py
@@ -1,15 +1,6 @@
-n = int(input("Enter a number: "))
-result = 0
-
-
 def factorial(n):
-    global result
     if n == 0:
         result = 1
     else:
         result = n * factorial(n-1)
     return result
-
-
-print(result)
-factorial(n)
