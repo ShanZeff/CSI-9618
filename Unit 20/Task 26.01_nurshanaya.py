ccb827d9e8d4a694a66a5dffb29450d8b8ecc554
ShanZeff <18shanzeff4@gmail.com>
2022-05-31 13:27:25 +0800

    new tasks

---
+++ Unit 20/Task 26.01_nurshanaya.py
@@ -0,0 +1,74 @@
+# Task 26.01 Record and Sequential file
+# 1. Write a complete program to save several car records to a
+#    sequential file
+# 2. Write another program to read the file and display the
+#    contents on screen
+
+import pickle   # this library is required to create binary files
+from datetime import date
+
+class CarRecord:
+    def __init__(self):
+        self.VehicleID = " "
+        self.Registration = " "
+        self.DateOfRegistration = date(1990, 1, 1)
+        self.EngineSize = 0
+        self.PurchasePrice = 0.0
+
+
+def SaveData(Car):
+    # file channel for car records
+    CarFile = open("CarFile.DAT", "wb")
+    for i in range(100):    # loop for each array element
+        # write a whole record to the binary file
+        pickle.dump(Car[i], CarFile)
+    CarFile.close()     # close file
+
+
+def LoadData():
+    CarFile = open("CarFile.DAT", "rb")    # open file for binary read
+    Car = []    # start with empty list
+    EoF = False
+    while not EoF:      # check for end of file
+        try:
+            Car.append(pickle.load(CarFile))  # append record from file to end of list
+        except:
+            EoF = True
+    CarFile.close()
+    return Car
+
+
+def OutputRecords(Car):
+    for i in range(100):    # loop for each array element
+        print(Car[i].VehicleID)     # write one field
+
+
+def main():
+    ThisCar = CarRecord()
+    Car = [ThisCar for i in range(100)]     # only run this 1st time
+    SaveData(Car)           # only run this 1st time
+    Car = LoadData()        # from existing file
+    # OutputRecords(Car)
+    # add more records
+    i = int(input("Record Number?: "))
+    while i != 0:
+        Car[i].VehicleID = input("Vehicle ID: ")
+        Car[i].Registration = input("Registration: ")
+        Car[i].DateOfRegistration = input("Registration Date: ")
+        Car[i].EngineSize = int(input("Engine size: "))
+        Car[i].PurchasePrice = float(input("Purchase price: "))
+        i = int(input("next Record Number?: "))
+    OutputRecords(Car)
+    SaveData(Car)
+
+
+main()
+
+
+# exception handling
+EoF = False
+while not EoF:
+    try:
+        Car.append(pickle.load(CarFile))
+    except:
+        EoF = True
---
+++ Unit 20/Task 26.02_nurshanaya.py
@@ -0,0 +1,97 @@
+# Task 26.02 Record and Random-access file
+# 1. Write a complete program to save several car records to a random-access file.
+# Write another program to find a record in the random-access file using the record key.
+# Display the record data on screen.
+
+import pickle   # this library is required to create binary files
+from datetime import date
+RECORDSIZE = 50     # 20 + 10 + 8 + 4 + 8
+class CarRecord:
+    def __int__(self):
+        VehicleID = "dummy"
+        VehicleID = VehicleID.ljust(20)
+        self.VehicleID = VehicleID.encode("utf-8")
+        Registration = " "
+        Registration = Registration.ljust(10)
+        self.Registration = Registration.encode("utf-8")
+        self.DateOfRegistration = date(1990, 1, 1)
+        self.EngineSize = 0
+        self.PurchasePrice = 0.0
+
+
+def InitialiseFile():
+    CarFile = open("CarFile.DAT", "wb")     # file for car records
+    for i in range(100):    # loop for each array element
+        Address = i * RECORDSIZE + 1
+        CarFile.seek(Address, 0)
+        # write a whole record to the binary file
+        pickle.dump(CarRecord(), CarFile)
+    CarFile.close()     # close file
+
+
+def InputNewRecordData():
+    ThisCar = CarRecord()
+    VehicleID = input("Vehicle ID: ")
+    VehicleID = VehicleID.ljust(20)
+    ThisCar.VehicleID = VehicleID.encode("utf-8")
+    Registration = input("Registration: ")
+    Registration = Registration.ljust(10)
+    ThisCar.Registration = Registration.encode("utf-8")
+    ThisCar.DateOfRegistration = (input("Registration Date: "))
+    ThisCar.EngineSize = int(input("Engine size: "))
+    ThisCar.PurchasePrice = float(input("Purchase price: "))
+    return ThisCar
+
+
+def Hash(reg):
+    result = ord(reg[0]) * RECORDSIZE + 1
+    print("Hashed to", result)
+    return result
+
+
+def SaveToFile(ThisCar, CarFile):
+    Address = Hash(ThisCar.Registration.decode("utf-8"))
+    CarFile.seek(Address, 0)
+    pickle.dump(ThisCar, CarFile)   # write a whole record to the binary file
+
+
+def OpenFileForUpdate():
+    CarFile = open("CarFile.DAT", "rb+")    # open file for update
+    return CarFile
+
+
+def FindRecord(reg, CarFile):
+    Address = Hash(reg)
+    CarFile.seek(Address, 0)
+    ThisCar = pickle.load(CarFile)      # load record from file
+    return ThisCar
+
+
+def OutputData(ThisCar):
+    print(ThisCar.VehicleID)    # write one field
+
+
+def main():
+    InitialiseFile()    # only run this procedure the 1st time
+    CarFile = OpenFileForUpdate()
+    ThisCar = CarRecord()
+
+    # add records
+    Answer = input("add a record? (Y/N) ")
+    while Answer != "N":
+        ThisCar = CarRecord()
+        ThisCar = InputNewRecordData()
+        SaveToFile(ThisCar, CarFile)
+        Answer = input("add a record? (Y/N) ")
+
+    # find records
+    Answer = input("find a record? (Y/N) ")
+    while Answer != "N":
+        Reg = input("Give vehicle registration: ")
+        ThisCar = FindRecord(Reg, CarFile)
+        OutputData(ThisCar)
+        Answer = input("find a record? (Y/N) ")
+    CarFile.close()
+
+
+main()
