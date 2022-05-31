0b1d898467112aa81ef95f6e8e647db3d23f622c
ShanZeff <18shanzeff4@gmail.com>
2022-05-31 21:30:04 +0800

    update

---
+++ Unit 20/Task 27.01.02_nurshanaya.py
@@ -0,0 +1,27 @@
+# Task 27.01: 2 OOP Instantiation
+# A business wants to store data about companies they supply
+# The data to be stored: company name, email address, date of last contact
+
+from datetime import *
+class Company:
+    def __init__(self, n, e):   # constructor
+        self.__CompanyName = n
+        self.__EmailAddress = e
+        self.__DateOfLastContact = None
+
+    def SetDateOfLastContact(self, d):
+        self.__DateOfLastContact = d
+
+    def GetCompanyName(self):
+        return self.__CompanyName
+
+    def GetEmailAddress(self):
+        return self.__EmailAddress
+
+    def GetDateOfLastContact(self):
+        return self.__DateOfLastContact
+
+
+ThisCompany = Company("SLimited", "abc@slimited.cie")
+ThisCompany.SetDateOfLastContact(date(2016, 2, 1))
+print(ThisCompany.GetDateOfLastContact())
