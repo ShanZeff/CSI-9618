14731024ab16320a5c44687da3a6da3fc12ca649
ShanZeff <18shanzeff4@gmail.com>
2022-06-07 00:07:13 +0800

    update

--- Unit 20/Task 27.08_nurshanaya.py
+++ Unit 20/Task 27.08_nurshanaya.py
@@ -1,19 +1,12 @@
-class Assessment:
-    def __init__(self, t, m):
-        self.__AssessmentTitle = t
-        self.__MaxMarks = m
-
-    def OutputAssessmentDetails(self):
-        print(self.__AssessmentTitle, " Marks: ", self.__MaxMarks)
-
+# Task 27.08 Containment
 
 class Course:
     def __init__(self, t, m):   # sets up a new course
-        self.__CourseTitle = t
-        self.__MaxStudents = m
-        self.__NumberOfLessons = 0
-        self.__CourseLesson = []
-        self.__CourseAssessment = Assessment
+        self.__CourseTitle = t                  # DECLARE CourseTitle       : STRING
+        self.__MaxStudents = m                  # DECLARE MaxStudents       : INTEGER
+        self.__NumberOfLessons = 0              # DECLARE NumberOfLessons   : INTEGER
+        self.__CourseLesson = []                # DECLARE CourseLesson[1:50] OF Lesson
+        self.__CourseAssessment = Assessment    # DECLARE CourseAssessment  : Assessment
 
     def AddLesson(self, t, d, r):
         self.__NumberOfLessons = self.__NumberOfLessons + 1
@@ -31,12 +24,21 @@ class Course:
 
 class Lesson:
     def __init__(self, t, d, r):
-        self.__LessonTitle = t
-        self.__DurationMinutes = d
-        self.__RequiresLab = r
+        self.__LessonTitle = t      # DECLARE LessonTitle       : STRING
+        self.__DurationMinutes = d  # DECLARE DurationMinutes   : INTEGER
+        self.__RequiresLab = r      # DECLARE RequiresLab       : BOOLEAN
 
     def OutputLessonDetails(self):
         print(self.__LessonTitle, self.__DurationMinutes)
+
+
+class Assessment:
+    def __init__(self, t, m):
+        self.__AssessmentTitle = t  # DECLARE AssessmentTitle   : STRING
+        self.__MaxMarks = m         # DECLARE MaxMarks          : INTEGER
+
+    def OutputAssessmentDetails(self):
+        print(self.__AssessmentTitle, " Marks: ", self.__MaxMarks)
 
 
 def main():
