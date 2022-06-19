569e6842de38a6de5469bfafb248d163e2473d97
ShanZeff <18shanzeff4@gmail.com>
2022-06-20 01:23:32 +0800

    update

--- Y21/y21_question1.py
+++ Y21/y21_question1.py
@@ -1,15 +1,47 @@
+# NumberOfItems = 9
+# TheData = [20, 3, 4, 8, 12, 99, 4, 26, 4]
+
+# def InsertionData(TheData):
+#    for Count in range(FirstElement, NumberOfItems - 1):
+#        Data
+
 TheData = [20, 3, 4, 8, 12, 99, 4, 26, 4]
-Count = 0
 
 def InsertionSort(TheData):
-    for Count in range(1):
-        DataToInsert = TheData[i]
+    for Count in range(0, len(TheData)):
+        DataToInsert = TheData[Count]
         Inserted = 0
         NextValue = Count - 1
-        while NextValue >= 0:
+        while NextValue >= 0 and Inserted != 1:
             if DataToInsert < TheData[NextValue]:
-                TheData[NextValue + 1] = TheData[NextValue]
+                TheData[NextValue+1] = TheData[NextValue]
                 NextValue = NextValue - 1
-                TheData[NextValue + 1] = TheData
+                TheData[NextValue+1] = DataToInsert
             else:
                 Inserted = 1
+
+
+def PrintArray(TheData):
+    for Count in range(0, len(TheData)):
+        print(TheData[Count])
+
+
+def Search(TheData):
+    NumberInput = int(input("Enter a whole number: "))
+    for Count in range(0, len(TheData)):
+        if TheData[Count] == NumberInput:
+            print("Found")
+            return True
+    print("False")
+    return False
+
+
+def main():
+    print("Before")
+    PrintArray(TheData)
+    InsertionSort(TheData)
+    print("After")
+    PrintArray(TheData)
+
+
+main()
