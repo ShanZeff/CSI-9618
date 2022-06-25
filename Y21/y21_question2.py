74cf2dbf686f8b3fcb557aece743d2a801631477
ShanZeff <18shanzeff4@gmail.com>
2022-06-25 15:36:10 +0800

    update

--- Y21/y21_question2.py
+++ Y21/y21_question2.py
@@ -1,10 +1,10 @@
 class HiddenBox:
     # __BoxName String
     # __Creator String
-    # __DateHidden String           Date
+    # __DateHidden String
     # __GameLocation String
     # __LastFinds [10] [2] String
-    # __Activate String             Boolean
+    # __Active Boolean
 
     def __init__(self, NewBoxName, NewCreator, NewDateHidden, NewLocation):
         self.__BoxName = NewBoxName
@@ -12,7 +12,7 @@ class HiddenBox:
         self.__DateHidden = NewDateHidden
         self.__GameLocation = NewLocation
         self.__LastFinds = [["" for j in range(0, 2)] for i in range(0, 10)]
-        self.__Activate = False
+        self.__Active = False
 
     def GetBoxName(self):
         return self.__BoxName
@@ -21,7 +21,7 @@ class HiddenBox:
         return self.__GameLocation
 
 
-TheBoxes = [HiddenBox("", "", "") for i in range(0, 10000)]
+TheBoxes = [HiddenBox("", "", "", "") for i in range(0, 10000)]
 
 def NewBox(TheBoxes, NumBoxes):
     BoxName = input("Enter the name of the box: ")
@@ -29,10 +29,12 @@ def NewBox(TheBoxes, NumBoxes):
     DateHidden = input("Enter the date the box was hidden: ")
     GameLocation = input("Enter the location of the box: ")
     TheBoxes[NumBoxes] = HiddenBox(BoxName, Creator, DateHidden, GameLocation)
+
+    NumBoxes = NewBox(TheBoxes, NumBoxes)
     return NumBoxes + 1
 
 
-NumBoxes = NewBox(TheBoxes, NumBoxes)
+NewBox(TheBoxes, NumBoxes)
 
 class PuzzleBox(HiddenBox):
     # __PuzzleText String
--- Y21/y21_question3.py
+++ Y21/y21_question3.py
@@ -1,3 +1,5 @@
+import os
+
 QueueData = ["" for i in range(0, 20)]
 StartPointer = 0
 EndPointer = 0
@@ -10,3 +12,42 @@ def Enqueue(DataToAdd, QueueData, EndP):
         EndP = EndP + 1
         return True, EndP
 
+
+def ReadFile(QueueData, StartP, EndP):
+    Path = "C:/Users/shnaya/PycharmProjects/CSI-9618/Y21/" + input("Enter a filename: ")
+    FileName = os.path.isfile(Path)
+    if os.path.isfile(FileName):
+        f = open(FileName, "r")
+        Flag = True
+        DataToInsert = (f.readline()).strip()
+        while Flag == True and DataToInsert != null:
+            Flag, EndP = Enqueue(QueueData, EndP)
+            DataToInsert = (f.readline()).strip()
+        if not Flag:
+            f.close()
+            return 1, EndP
+        else:
+            f.close()
+            return 2, EndP
+    else:
+        return -1, EndP
+
+
+ReturnValue, EndPointer = ReadFile(QueueData, StartPointer, EndPointer)
+if ReturnValue == -1:
+    print("The file could not be found")
+elif ReturnValue == 1:
+    print("The queue was full, not all items were read")
+else:
+    print("All items successfully added")
+
+
+def Remove(QueueData, StartP, EndP):
+    if EndP - StartP < 2:
+        return "No Items", StartP, EndP
+    else:
+        Value1 = QueueData[StartP]
+        StartP = StartP + 1
+        Value2 = QueueData[StartP]
+        StartP = StartP + 1
+        return (Value1 + " " + Value2), StartP, EndP
