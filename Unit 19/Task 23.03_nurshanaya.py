20860591619e1368f538bd5db5488456d15dc5d9
ShanZeff <18shanzeff4@gmail.com>
2022-07-14 21:25:49 +0800

    update

--- Unit 19/Task 23.03_nurshanaya.py
+++ Unit 19/Task 23.03_nurshanaya.py
@@ -1,23 +1,20 @@
+# Insertion Sort
+List = [0, 53, 21, 60, 18, 42, 19]
 NumberOfItems = 6
-List = [0]
-List.append(53)
-List.append(21)
-List.append(60)
-List.append(18)
-List.append(42)
-List.append(19)
 
 for Pointer in range(1, NumberOfItems + 1):
-    print(List[Pointer], end=' ')
-print();
+    print(List[Pointer], end='')
+    print()
+
 
 for Pointer in range(2, NumberOfItems + 1):
     ItemToBeInserted = List[Pointer]
-    CurrentItem = Pointer -1
-    while (List[CurrentItem] > ItemToBeInserted) and (CurrentItem > 0):
+    CurrentItem = Pointer - 1
+    while List[CurrentItem] > ItemToBeInserted and CurrentItem > 0:
         List[CurrentItem + 1] = List[CurrentItem]
         CurrentItem = CurrentItem - 1
         List[CurrentItem + 1] = ItemToBeInserted
+
 
 for Pointer in range(1, NumberOfItems + 1):
     print(List[Pointer], end=' ')
--- Unit 19/Task 23.04_nurshanaya.py
+++ Unit 19/Task 23.04_nurshanaya.py
@@ -1,24 +1,27 @@
+# Binary Search
 Found = False
 SearchFailed = False
+List = [7, 12, 19, 23, 27, 33, 37, 41, 45, 56, 59, 60, 62, 71, 75, 80, 84, 88, 92, 99]
+MaxItems = 20
 First = 0
 Last = MaxItems - 1     # set boundaries of search area
-List = [7, 12, 19, 23, 27, 33, 37, 41, 45, 56, 59, 60, 62, 71, 75, 80, 84, 88, 92, 99]
 SearchItem = int(input("What number are you searching for?: "))
 
+
 while not Found and not SearchFailed:
-    Middle = (First + Last) / 2     # find middle of current search area
+    Middle = int((First + Last) / 2)     # find middle of current search area
     if List[Middle] == SearchItem:
         Found = True
     else:
         if First >= Last: # no search area left
             SearchFailed = True
         else:
-            if List[Middle] > SearhItem:
+            if List[Middle] > SearchItem:
                 Last = Middle - 1
             else:
                 First = Middle + 1
 
 if Found == True:
-    print(List[Middle])
+    print(Middle)
 else:
     print("Item not present in array")
