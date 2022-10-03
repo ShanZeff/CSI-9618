de57672a691e6c64ef0ee8dcc4421b6d30c78d9a
ShanZeff <18shanzeff4@gmail.com>
2022-10-03 22:05:42 +0800

    update

--- Revision/june41_q1.py
+++ Revision/june41_q1.py
@@ -1,2 +1,27 @@
 # June 41 2022: Q1
 # Text file
+
+FileData = [[""] * 2 for i in range(11)]  # STRING
+
+
+def ReadHighScores():
+    Filename = "HighScore.txt"
+    File = open(Filename, 'r')
+    for x in range(0, 10):
+        FileData[x][0] = File.readline()[:3]
+        FileData[x][1] = File.readline()
+    File.close()
+
+
+def OutputHighScores():
+    for x in range(0, 11):
+        Output = FileData[x][0] + " " + FileData[x][1]
+        print(Output)
+
+
+def main():
+    ReadHighScores()
+    OutputHighScores()
+
+
+main()
