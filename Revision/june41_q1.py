2aee7a185e179a00a105104c9975f71197427609
ShanZeff <18shanzeff4@gmail.com>
2022-10-06 00:22:13 +0800

    update

--- Revision/june41_q1.py
+++ Revision/june41_q1.py
@@ -5,7 +5,7 @@ FileData = [[""] * 2 for i in range(11)]  # STRING
 
 
 def ReadHighScores():
-    Filename = "HighScore.txt"
+    Filename = "C:/Users/shnaya/Desktop/9618 Practical/9618_sum22_sf_41/HighScore.txt"
     File = open(Filename, 'r')
     for x in range(0, 10):
         FileData[x][0] = File.readline()[:3]
@@ -19,8 +19,50 @@ def OutputHighScores():
         print(Output)
 
 
+def Arrange(Username, Score):
+    for x in range(0, 10):
+        if Score > int(FileData[x][1]):
+            Temp1 = FileData[x][0]
+            Temp2 = FileData[x][1]
+            FileData[x][0] = Username
+            FileData[x][1] = Score
+            Count = x + 1
+            while Count < 10:
+                Second1 = FileData[Count][0]
+                Second2 = FileData[Count][1]
+                FileData[Count][0] = Temp1
+                FileData[Count][1] = Temp2
+
+                Temp1 = Second1
+                Temp2 = Second2
+                Count = Count + 1
+                break
+
+
+def WriteTopTen():
+    Filename = "NewHighScore.txt"
+    Filename = open(Filename, 'w')
+    for x in range(0, 10):
+        Filename.write(str(FileData[x][0]) + '\n')
+        Filename.write(str(FileData[x][1]) + '\n')
+        Filename.close
+
+
 def main():
     ReadHighScores()
+    OutputHighScores()
+    # Username = "ABCD"
+    # while len(Username) != 3:
+    #     Username = input("Enter your Username: ")
+    # Score = -1
+    # while Score < 1 or Score > 100000:
+    #     Score = int(input("Enter score:"))
+
+    Username = input("Enter your Username: ")
+    Score = -1
+    while Score < 0 or Score > 100000:
+        Score = int(input("Enter score: "))
+    Arrange(Username, Score)
     OutputHighScores()
 
 
