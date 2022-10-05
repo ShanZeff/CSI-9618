276efa6009cb94b1b01b5f9570ed52f04e743546
ShanZeff <18shanzeff4@gmail.com>
2022-10-06 00:55:10 +0800

    update

--- Revision/june42_q3.py
+++ Revision/june42_q3.py
@@ -1,2 +1,33 @@
 # June 42 2022: Q3
 # OOP
+
+class Card:
+    # Private Number : INTEGER
+    # Private Colour : STRING
+    def __init__(self, NumberP, ColourP):
+        self.__Number = NumberP
+        self.__Colour = ColourP
+
+    def GetNumber(self):
+        return self.__Number
+
+    def GetColour(self):
+        return self.__Colour
+
+
+def main():
+    CardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # INTEGER
+
+    try:
+        Filename = "CardValues.txt"
+        File = open(Filename, 'r')
+        for x in range(0, 30):
+            NumberRead = int(File.readline())
+            ColourRead = File.readline()
+            CardArray[x] = Card(NumberRead, ColourRead)
+            File.close()
+    except IOError:
+        print("Could not find file")
+
+
+main()
